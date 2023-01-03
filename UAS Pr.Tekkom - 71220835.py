7a58567172586a41f7fa39d179961d46fa2b625a
Alvionaputri07 <117884597+Alvionaputri07@users.noreply.github.com>
2023-01-03 12:18:36 +0700

    Add files via upload

---
+++ UAS Pr.Tekkom - 71220835.py
@@ -0,0 +1,14 @@
+***** Kredit Keaktifan Mahasiswa *****
+print(Student Activities Credit)
+print 1. Menambahkan Kegiatan
+print 2. Menampikan Jumlah Point
+print 3. Keluar
+
+def("Silahkan masukan pilihan anda: ")
+    print"Nama kegiatan: "
+    print"Tanggal Kegiatan: "
+    print"Pilihan Kategori Kegiatan: "
+    print"Prestasi"
+    print"Organisasi
+    print"kepanitiaan
+   
